8b8be747c5db95f03d477e750e32535f68e5545e
ckellydev <c.kelly.dev@gmail.com>
2015-11-13 16:47:40 +0000

    Pulled Tweets

--- TweetAnalyser/src/tweetanalyser/TweetAnalyser_Main.java
+++ TweetAnalyser/src/tweetanalyser/TweetAnalyser_Main.java
@@ -23,24 +23,34 @@ public class TweetAnalyser_Main {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        Twitter twitter = null;
         
   try {
-            ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-          .setOAuthConsumerKey("qnL70f170zly7rCl73OfLIaHk")
-          .setOAuthConsumerSecret("QVR1RnW6mK9fWyDQJSQy9lWOvpCd9RTVapM76ym5B1OTJNKMDt")
-          .setOAuthAccessToken("4172361316-o8VJSIyiE3WBtr5gfHUziNM9UuYSwdTk2LHPkCF")
-          .setOAuthAccessTokenSecret("5W8yCqnrnJJzNEaS6e904tbs2FvU8NEDeOqHBkB1Rjz9k");
+           ConfigurationBuilder cb = new ConfigurationBuilder();
+           cb.setDebugEnabled(true)
+            .setOAuthConsumerKey("qnL70f170zly7rCl73OfLIaHk")
+            .setOAuthConsumerSecret("QVR1RnW6mK9fWyDQJSQy9lWOvpCd9RTVapM76ym5B1OTJNKMDt")
+            .setOAuthAccessToken("4172361316-o8VJSIyiE3WBtr5gfHUziNM9UuYSwdTk2LHPkCF")
+            .setOAuthAccessTokenSecret("5W8yCqnrnJJzNEaS6e904tbs2FvU8NEDeOqHBkB1Rjz9k");
             TwitterFactory factory = new TwitterFactory(cb.build());
-            Twitter twitter = factory.getInstance();
+            twitter = factory.getInstance();
            
             String screenName = twitter.getScreenName();
             System.out.println(screenName + " Logged In!");
 
+            Query query = new Query("Fallout4");
+            QueryResult result = twitter.search(query);
+            
+            for (Status status : result.getTweets()){
+                System.out.println(status.getCreatedAt() + " " + status.getText() + " " + status.getId());
+            }
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+  
     }
+  
+        
     
 }
